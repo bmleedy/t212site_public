526f24bf394deac0eee27ed0e3fed96af26f2cb1
Brett Leedy <bmleedy@fb.com>
2026-01-09 17:04:43 -0800

    added db schema for future work

---
+++ db_copy/db_structure_only.sql
@@ -0,0 +1,574 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 10, 2026 at 01:02 AM
+-- Server version: 11.8.3-MariaDB-log
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `u104214272_t212`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_log`
+--
+
+CREATE TABLE `activity_log` (
+  `timestamp` datetime(6) NOT NULL COMMENT 'server-side timestamp of when this log was written.',
+  `source_file` varchar(200) NOT NULL COMMENT 'Name of file which wrote this line.  __FILE__',
+  `action` varchar(100) NOT NULL COMMENT 'Human-readable brief name of what action was requested.',
+  `values_json` varchar(500) DEFAULT NULL COMMENT 'json blob of user-inputted data.  500 characters max.',
+  `success` tinyint(1) DEFAULT NULL COMMENT 'True=successful action.',
+  `freetext` varchar(500) DEFAULT NULL COMMENT 'user-friendly explanation/context.  500 char max',
+  `user` int(11) NOT NULL COMMENT 'REQUIRED user/actor field.  integer user id from the users table.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_daily`
+--
+
+CREATE TABLE `attendance_daily` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `was_present` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `committee`
+--
+
+CREATE TABLE `committee` (
+  `role_id` int(11) NOT NULL COMMENT 'auto-incrementing unique id',
+  `role_name` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `sort_order` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(6) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `startdate` datetime NOT NULL,
+  `enddate` datetime NOT NULL,
+  `cost` decimal(5,2) NOT NULL,
+  `adult_cost` decimal(5,2) NOT NULL,
+  `aic_id` int(11) DEFAULT NULL,
+  `sic_id` int(11) DEFAULT NULL,
+  `reg_open` smallint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_types`
+--
+
+CREATE TABLE `event_types` (
+  `id` int(11) NOT NULL,
+  `label` varchar(20) NOT NULL,
+  `sort` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `families`
+--
+
+CREATE TABLE `families` (
+  `family_id` int(11) NOT NULL,
+  `address1` varchar(40) NOT NULL,
+  `address2` varchar(40) DEFAULT NULL,
+  `city` varchar(25) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `zip` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gear`
+--
+
+CREATE TABLE `gear` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `id` int(11) NOT NULL,
+  `reg_ids` varchar(30) NOT NULL,
+  `cost` decimal(5,2) NOT NULL,
+  `token` varchar(80) NOT NULL,
+  `status` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leadership`
+--
+
+CREATE TABLE `leadership` (
+  `id` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL,
+  `sort` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mb_counselors`
+--
+
+CREATE TABLE `mb_counselors` (
+  `id` int(11) NOT NULL,
+  `mb_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mb_list`
+--
+
+CREATE TABLE `mb_list` (
+  `id` int(11) NOT NULL,
+  `mb_name` varchar(40) NOT NULL,
+  `eagle_req` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `page_counters`
+--
+
+CREATE TABLE `page_counters` (
+  `id` int(11) NOT NULL,
+  `page_url` varchar(255) NOT NULL,
+  `count` int(11) NOT NULL DEFAULT 1,
+  `first_visit` datetime NOT NULL,
+  `last_visit` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrols`
+--
+
+CREATE TABLE `patrols` (
+  `id` int(11) NOT NULL,
+  `label` varchar(30) NOT NULL,
+  `sort` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phone`
+--
+
+CREATE TABLE `phone` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ranks`
+--
+
+CREATE TABLE `ranks` (
+  `id` int(11) NOT NULL,
+  `label` varchar(12) NOT NULL,
+  `sort` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recharter`
+--
+
+CREATE TABLE `recharter` (
+  `id` int(11) NOT NULL,
+  `scout_id` int(11) NOT NULL,
+  `pp_token` varchar(48) NOT NULL,
+  `boyslife` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `approved_by` int(11) NOT NULL,
+  `paid` tinyint(1) NOT NULL,
+  `attending` tinyint(1) NOT NULL DEFAULT 1,
+  `nbrInGroup` int(2) NOT NULL,
+  `seat_belts` int(2) NOT NULL,
+  `seat_belts_return` int(2) NOT NULL,
+  `drive` varchar(4) NOT NULL,
+  `ts_register` datetime NOT NULL,
+  `ts_approved` datetime NOT NULL,
+  `ts_paid` datetime NOT NULL,
+  `spec_instructions` varchar(512) NOT NULL,
+  `pp_token` varchar(48) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relationships`
+--
+
+CREATE TABLE `relationships` (
+  `id` int(11) NOT NULL,
+  `scout_id` int(11) NOT NULL,
+  `adult_id` int(11) NOT NULL,
+  `type` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scout_info`
+--
+
+CREATE TABLE `scout_info` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rank_id` int(11) NOT NULL,
+  `patrol_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  `rechartered` tinyint(1) DEFAULT 0,
+  `boyslife` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
+  `family_id` int(11) DEFAULT NULL,
+  `rechartered` tinyint(4) DEFAULT 0,
+  `user_name` varchar(64) NOT NULL COMMENT 'user''s name, unique',
+  `user_password_hash` varchar(255) NOT NULL COMMENT 'user''s password in salted and hashed format',
+  `user_email` varchar(64) NOT NULL COMMENT 'user''s email, unique',
+  `user_active` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'user''s activation status',
+  `user_activation_hash` varchar(40) DEFAULT NULL COMMENT 'user''s email verification hash string',
+  `user_password_reset_hash` char(40) DEFAULT NULL COMMENT 'user''s password reset code',
+  `user_password_reset_timestamp` bigint(20) DEFAULT NULL COMMENT 'timestamp of the password reset request',
+  `user_rememberme_token` varchar(64) DEFAULT NULL COMMENT 'user''s remember-me cookie token',
+  `user_failed_logins` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'user''s failed login attemps',
+  `user_last_failed_login` int(10) DEFAULT NULL COMMENT 'unix timestamp of last failed login attempt',
+  `user_registration_datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_registration_ip` varchar(39) NOT NULL DEFAULT '0.0.0.0',
+  `user_first` varchar(40) NOT NULL,
+  `user_last` varchar(40) NOT NULL,
+  `is_scout` tinyint(1) NOT NULL,
+  `user_type` varchar(7) NOT NULL,
+  `user_access` varchar(20) NOT NULL,
+  `notif_preferences` varchar(200) DEFAULT NULL COMMENT 'JSON for notification preferences.'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci COMMENT='user data';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activity_log`
+--
+ALTER TABLE `activity_log`
+  ADD KEY `log_timestamp` (`timestamp`) USING BTREE;
+
+--
+-- Indexes for table `attendance_daily`
+--
+ALTER TABLE `attendance_daily`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `date` (`date`);
+
+--
+-- Indexes for table `committee`
+--
+ALTER TABLE `committee`
+  ADD PRIMARY KEY (`role_id`),
+  ADD UNIQUE KEY `unique_committee_sort_order` (`sort_order`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `event_types`
+--
+ALTER TABLE `event_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `families`
+--
+ALTER TABLE `families`
+  ADD PRIMARY KEY (`family_id`);
+
+--
+-- Indexes for table `gear`
+--
+ALTER TABLE `gear`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `leadership`
+--
+ALTER TABLE `leadership`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mb_counselors`
+--
+ALTER TABLE `mb_counselors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mb_list`
+--
+ALTER TABLE `mb_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `page_counters`
+--
+ALTER TABLE `page_counters`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `page_url` (`page_url`);
+
+--
+-- Indexes for table `patrols`
+--
+ALTER TABLE `patrols`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `phone`
+--
+ALTER TABLE `phone`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ranks`
+--
+ALTER TABLE `ranks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `recharter`
+--
+ALTER TABLE `recharter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `relationships`
+--
+ALTER TABLE `relationships`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `scout_info`
+--
+ALTER TABLE `scout_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`),
+  ADD UNIQUE KEY `user_email` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance_daily`
+--
+ALTER TABLE `attendance_daily`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `committee`
+--
+ALTER TABLE `committee`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto-incrementing unique id';
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `event_types`
+--
+ALTER TABLE `event_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `families`
+--
+ALTER TABLE `families`
+  MODIFY `family_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `gear`
+--
+ALTER TABLE `gear`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `leadership`
+--
+ALTER TABLE `leadership`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mb_counselors`
+--
+ALTER TABLE `mb_counselors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mb_list`
+--
+ALTER TABLE `mb_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `page_counters`
+--
+ALTER TABLE `page_counters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patrols`
+--
+ALTER TABLE `patrols`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phone`
+--
+ALTER TABLE `phone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ranks`
+--
+ALTER TABLE `ranks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `recharter`
+--
+ALTER TABLE `recharter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `relationships`
+--
+ALTER TABLE `relationships`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `scout_info`
+--
+ALTER TABLE `scout_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
